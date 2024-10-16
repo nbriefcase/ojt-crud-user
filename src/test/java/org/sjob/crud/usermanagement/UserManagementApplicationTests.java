79b77c57ec311a0c37f35372137ec9adb136b31a
Neiro Diaz <gpbriefcase@gmail.com>
2024-10-16 13:43:25 -0500

    Initial Settings

---
+++ src/test/java/org/sjob/crud/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sjob.crud.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
